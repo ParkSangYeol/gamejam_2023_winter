f660402ade85df6db80fe5c3db12998502003e86
Neli3307 <yeduk33@gmail.com>
2023-02-19 09:30:50 +0900

    Add Battery Randomizer

---
+++ Disarm the bomb/Assets/BatteryRandomizer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatteryRandomizer : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> batteries;
+    public int activeBatteryNumber;
+
+    private void Start()
+    {
+        activeBatteryNumber = Random.Range(0, 5);
+        for(int i = 4; i > activeBatteryNumber; i--)
+        {
+            batteries[i - 1].SetActive(false);
+        }
+    }
+}
