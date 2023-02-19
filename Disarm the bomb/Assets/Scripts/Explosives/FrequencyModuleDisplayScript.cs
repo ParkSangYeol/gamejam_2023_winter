32890a9003c4502224a1c28f77afc1a86a6a91a1
I_Will_Be_Expert <tkddufdl77@ajou.ac.kr>
2023-02-19 19:53:15 +0900

    Update Frequency Module

--- Disarm the bomb/Assets/Scripts/Explosives/FrequencyModuleDisplayScript.cs
+++ Disarm the bomb/Assets/Scripts/Explosives/FrequencyModuleDisplayScript.cs
@@ -19,9 +19,9 @@ public class FrequencyModuleDisplayScript : Puzzle
     private Material silverMaterial;
 
     private List<float> FrequencyList = new List<float> { 88.9f, 90.1f, 91.3f, 91.5f, 91.9f, 94.5f, 95.3f, 97.5f, 98.3f, 98.7f, 98.9f, 99.1f, 99.7f, 100.5f, 102.3f };
-    private List<string> leftResisterList = new List<string> { "rrybs", "rybsg", "brygs", "rybgs", "srgby", "bgyrs", "ybsrg", "sbryg", "ysrgb", "rsybg", "brysg", "rygsb", "gybrs", "ybsgr", "rbysg" };
-    private List<string> RightResisterList = new List<string> { "gbyrs", "sbygr", "gysrb", "bygrs", "gyrsb", "rsgby", "grysb", "rbygs", "bgysr", "ygrbs", "gsryb", "sbyrg", "rgbsy", "bgsyr", "ysbrg" };
-    private List<int> correctIndex = new List<int> {4,3,5,1,2};
+    private List<string> leftResisterList = new List<string> { "GYBS", "YGBS", "RYGS", "BYRS", "GYRS", "BRYG", "YRGS", "RGYS", "BSYR", "RBYG", "YBRS", "GYSR", "RYBG", "BGYS", "SYRG" };
+    private List<string> RightResisterList = new List<string> { "YRBS", "RSGY", "SBRY", "BYGS", "GRYB", "YRSG", "RGSY", "BGYR", "YSRG", "RSGB", "BSGY", "YGBR", "GSRB", "RSBY", "SYBR" };
+    private List<int> correctIndex = new List<int> {3,4,1,2};
     private TMP_Text tmpText;
     private int currentIndex;
     private int answerIndex;
@@ -39,62 +39,58 @@ public class FrequencyModuleDisplayScript : Puzzle
 
         Material[] materials = new Material[6];
         materials[0] = resisterBaseMaterial;
-        for (int i = 0; i < 5; i++)
+        materials[5] = silverMaterial;
+        for (int i = 0; i < 4; i++)
         {
-            if (leftResisterList[answerIndex][i] == 'r')
+            if (leftResisterList[answerIndex][i] == 'R')
             {
                 materials[correctIndex[i]] = redMaterial;
             }
-            else if (leftResisterList[answerIndex][i] == 'y')
+            else if (leftResisterList[answerIndex][i] == 'Y')
             {
                 materials[correctIndex[i]] = yellowMaterial;
             }
-            else if (leftResisterList[answerIndex][i] == 'g')
+            else if (leftResisterList[answerIndex][i] == 'G')
             { 
                 materials[correctIndex[i]] = greenMaterial;
             }
-            else if (leftResisterList[answerIndex][i] == 'b')
+            else if (leftResisterList[answerIndex][i] == 'B')
             {
                 materials[correctIndex[i]] = blueMaterial;
             }
-            else if (leftResisterList[answerIndex][i] == 's')
+            else if (leftResisterList[answerIndex][i] == 'S')
             {
                 materials[correctIndex[i]] = silverMaterial;
             }
         }
-        
-        for (int i = 0; i < 6; i++)
+       
+        leftResister.materials = materials;
+   
+        for (int i = 0; i < 4; i++)
         {
-            leftResister.materials[i] = materials[i];
-        }
-        for (int i = 0; i < 5; i++)
-        {
-            if (RightResisterList[answerIndex][i] == 'r')
+            if (RightResisterList[answerIndex][i] == 'R')
             {
                 materials[correctIndex[i]] = redMaterial;
             }
-            else if (RightResisterList[answerIndex][i] == 'y')
+            else if (RightResisterList[answerIndex][i] == 'Y')
             {
                 materials[correctIndex[i]] = yellowMaterial;
             }
-            else if (RightResisterList[answerIndex][i] == 'g')
+            else if (RightResisterList[answerIndex][i] == 'G')
             {
                 materials[correctIndex[i]] = greenMaterial;
             }
-            else if (RightResisterList[answerIndex][i] == 'b')
+            else if (RightResisterList[answerIndex][i] == 'B')
             {
                 materials[correctIndex[i]] = blueMaterial;
             }
-            else if (RightResisterList[answerIndex][i] == 's')
+            else if (RightResisterList[answerIndex][i] == 'S')
             {
                 materials[correctIndex[i]] = silverMaterial;
             }
         }
         
-        for (int i = 0; i < 6; i++)
-        {
-            rightResister.materials[i] = materials[i];
-        }
+        rightResister.materials = materials;
 
         currentIndex = 0;
         tmpText.text = FrequencyList[currentIndex].ToString();
